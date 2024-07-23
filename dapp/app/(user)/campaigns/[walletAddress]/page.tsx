d49edbd41026ef20b1cc73591285115473e8a2b2
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-07-23 23:59:44 +0100

    Update page.tsx

--- dapp/app/(user)/campaigns/[walletAddress]/page.tsx
+++ dapp/app/(user)/campaigns/[walletAddress]/page.tsx
@@ -9,56 +9,68 @@ const page = () => {
   const { address } = useAccount();
   return (
     <section className="min-h-[100svh] py-[5rem] px-4 lg:px-[10vw]">
-      <Container className="flex flex-col gap-8">
-        <ConnectButton />
+      <Container className="flex flex-col justify-between  gap-8">
+        {/* <ConnectButton /> */}
         {!address ? (
           <h2>Connect to see your campaigns</h2>
         ) : (
           <h2>Your current campaigns</h2>
         )}
-
-        {!address ? (
-          <div className="grid gap-4 lg:grid-cols-3">
-            <CardLoader />
-            <CardLoader />
-            <CardLoader />
-          </div>
-        ) : (
-          <div className="grid gap-4  md:gap-8 lg:grid-cols-3 md:max-w-[800px] lg:max-w-none md:mx-auto  md:justify-center ">
-            <Card
-              cid={"QmVMdQGZEic4ByfJEXkD44t8XFokiDFT2FtGrt3pZtqsiH"}
-              causeName={"Stephanie need a house in Maitama" || "Unknown Cause"}
-              imageSrc={
-                `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}QmTHJNYBspAccj5BmGUbA34eoWTLsxHTgvauFbKsggS5Tb?pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_API_KEY}` ||
-                "/default-image.webp"
-              }
-              location={"Manchester, United Kingdom"}
-              progress={0}
-              token_id={"4"}
-              campaign_address={
-                "0x365c1ce0c51f98fc57cbdd225328ef3feb269b2c55a15da10bf4aa4c2fb75aa" ||
-                "0x0"
-              }
-              target={"1"}
-              url={`/campaign/${address}/1`}
-            />
-            {/* <Card
-              cid={"QmVMdQGZEic4ByfJEXkD44t8XFokiDFT2FtGrt3pZtqsiH"}
-              causeName={"Stephanie need a house in Maitama" || "Unknown Cause"}
-              imageSrc={
-                `${process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL}QmTHJNYBspAccj5BmGUbA34eoWTLsxHTgvauFbKsggS5Tb?pinataGatewayToken=${process.env.NEXT_PUBLIC_PINATA_API_KEY}` ||
-                "/default-image.webp"
-              }
-              location={"Manchester, United Kingdom"}
-              progress={0}
-              token_id={"4"}
-              campaign_address={
-                "0x365c1ce0c51f98fc57cbdd225328ef3feb269b2c55a15da10bf4aa4c2fb75aa" ||
-                "0x0"
-              }
-              target={"1"}
-            /> */}
-          </div>
+        <div className="grid gap-4  md:gap-8 lg:grid-cols-3 md:max-w-[800px] lg:max-w-none md:mx-auto  md:justify-center">
+          {loading && address
+            ? Array.from({ length: 3 }).map((_, idx) => (
+                <CardLoader key={idx} />
+              ))
+            : collections.map((data, idx) => {
+                return (
+                  <>
+                    <Card
+                      cid={data.cid}
+                      causeName={data.name || "Unknown Cause"}
+                      imageSrc={
+                        `${
+                          process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
+                        }${data.image?.slice(7, -1)}?pinataGatewayToken=${
+                          process.env.NEXT_PUBLIC_PINATA_API_KEY
+                        }` || "/default-image.webp"
+                      }
+                      location={data.location}
+                      progress={0}
+                      key={idx}
+                      token_id={data.id}
+                      campaign_address={data.campaign_address || "0x0"}
+                      target={data.target}
+                      url={`/campaign/${address}/1`}
+                    />
+                    <Card
+                      cid={data.cid}
+                      causeName={data.name || "Unknown Cause"}
+                      imageSrc={
+                        `${
+                          process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
+                        }${data.image?.slice(7, -1)}?pinataGatewayToken=${
+                          process.env.NEXT_PUBLIC_PINATA_API_KEY
+                        }` || "/default-image.webp"
+                      }
+                      location={data.location}
+                      progress={0}
+                      key={idx}
+                      token_id={data.id}
+                      campaign_address={data.campaign_address || "0x0"}
+                      target={data.target}
+                      url={`/campaign/${address}/1`}
+                    />
+                  </>
+                );
+              })}
+        </div>
+        {address && collections.length === 0 && (
+          <p className="flex items-center  gap-1">
+            <span>
+              <WarningIcon />
+            </span>
+            <span>You do not have any campaigns. Try creating one</span>
+          </p>
         )}
       </Container>
     </section>
