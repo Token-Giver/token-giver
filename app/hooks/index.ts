929ec969ca5fd0f3280aa6ad7227b325f21dade7
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2024-12-12 14:36:58 +0100

    Update index.ts

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -3,7 +3,11 @@ import {
   TBAImplementationAccount_SEPOLIA,
   TBAcontractAddress_SEPOLIA,
 } from "../utils/data";
-import { TokenboundClient } from "starknet-tokenbound-sdk";
+import {
+  TBAVersion,
+  TokenboundClient,
+  TokenboundClientOptions,
+} from "starknet-tokenbound-sdk";
 import { useEffect, useState } from "react";
 
 export const useTokenBoundSDK = () => {
@@ -14,6 +18,8 @@ export const useTokenBoundSDK = () => {
     registryAddress: TBAcontractAddress_SEPOLIA,
     implementationAddress: TBAImplementationAccount_SEPOLIA,
     jsonRPC: `https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/v0_7/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+    version: TBAVersion.V3,
+    chain_id: "SN_SEPOLIA",
   };
 
   let tokenbound: any;
