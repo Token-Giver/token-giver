7d6848cc300fa2d10c04f8d13ca2172b022987f4
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2024-12-17 16:57:12 +0100

    feat: develop explore page

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -6,6 +6,7 @@ import { campaign_contract } from "../utils/data";
 import { fetchCampaigns } from "../utils/helper";
 import Container from "../components/util/Container";
 import { H2 } from "../components/util/Headers";
+import Image from "next/image";
 
 const page = () => {
   const [cursor, setCursor] = useState(null);
@@ -36,54 +37,76 @@ const page = () => {
   return (
     <section className="min-h-screen">
       <Container className="mt-[5rem] py-10 px-4 md:px-10 lg:px-16">
-        <H2 style="my-[5rem]">Browse Fundraisers</H2>
-        <section
-          id="fundraisers"
-          className="grid gap-4  md:gap-8 lg:grid-cols-3 md:max-w-[800px] lg:max-w-none md:mx-auto  md:justify-center "
-        >
-          {loading
-            ? Array.from({ length: 12 }).map((_, idx) => (
-                <CardLoader key={idx} />
-              ))
-            : cachedCollections.map((nft, idx) => {
-                const {
-                  name,
-                  image,
-                  campaign_address,
-                  id,
-                  location,
-                  cid,
-                  target,
-                } = nft || {};
-                const path = name
-                  .replace(/[^a-zA-Z ]/g, "")
-                  .replace(/ /g, "-")
-                  .toLocaleLowerCase()
-                  .replace(/-+/g, "-");
+        <main>
+          <section>
+            <H2>
+              Browse Through different categories, to give a Helping hand.
+            </H2>
+            {/* style="my-[5rem]" */}
+            <p>
+              Merging Heart with Technology: Secure, Transparent Giving for
+              Maximum Impact
+            </p>
+            <button className="bg-[#127C56] text-white px-6 py-2 rounded-[25px]">
+              Start a Campaign
+            </button>
 
-                const url = `${path}/${campaign_address}/${cid}`;
-                return (
-                  <Card
-                    causeName={name || "Unknown Cause"}
-                    imageSrc={
-                      `${
-                        process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
-                      }${image.slice(7, -1)}?pinataGatewayToken=${
-                        process.env.NEXT_PUBLIC_PINATA_API_KEY
-                      }` || "/default-image.webp"
-                    }
-                    location={location}
-                    target={target}
-                    key={idx}
-                    progress={43}
-                    token_id={id}
-                    campaign_address={campaign_address || "0x0"}
-                    cid={cid}
-                    url={url}
-                  />
-                );
-              })}
-        </section>
+            <Image
+              src="/heart-pic.png"
+              alt="helping hand picture"
+              width={358}
+              height={270}
+              className="rounded-[2.5rem]"
+            />
+          </section>
+          {/* <section
+            id="fundraisers"
+            className="grid gap-4  md:gap-8 lg:grid-cols-3 md:max-w-[800px] lg:max-w-none md:mx-auto  md:justify-center "
+          >
+            {loading
+              ? Array.from({ length: 12 }).map((_, idx) => (
+                  <CardLoader key={idx} />
+                ))
+              : cachedCollections.map((nft, idx) => {
+                  const {
+                    name,
+                    image,
+                    campaign_address,
+                    id,
+                    location,
+                    cid,
+                    target,
+                  } = nft || {};
+                  const path = name
+                    .replace(/[^a-zA-Z ]/g, "")
+                    .replace(/ /g, "-")
+                    .toLocaleLowerCase()
+                    .replace(/-+/g, "-");
+
+                  const url = `${path}/${campaign_address}/${cid}`;
+                  return (
+                    <Card
+                      causeName={name || "Unknown Cause"}
+                      imageSrc={
+                        `${
+                          process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL
+                        }${image.slice(7, -1)}?pinataGatewayToken=${
+                          process.env.NEXT_PUBLIC_PINATA_API_KEY
+                        }` || "/default-image.webp"
+                      }
+                      location={location}
+                      target={target}
+                      key={idx}
+                      progress={43}
+                      token_id={id}
+                      campaign_address={campaign_address || "0x0"}
+                      cid={cid}
+                      url={url}
+                    />
+                  );
+                })}
+          </section> */}
+        </main>
       </Container>
     </section>
   );
---
+++ app/utils/category.ts
@@ -0,0 +1,42 @@
+const category = [
+  {
+    icon: "",
+    title: "Medical",
+  },
+  {
+    icon: "",
+    title: "Emergency",
+  },
+  {
+    icon: "",
+    title: "Family",
+  },
+  {
+    icon: "",
+    title: "Events",
+  },
+  {
+    icon: "",
+    title: "Sports",
+  },
+  {
+    icon: "",
+    title: "Creative",
+  },
+  {
+    icon: "",
+    title: "Volunteer",
+  },
+  {
+    icon: "",
+    title: "Travel",
+  },
+  {
+    icon: "",
+    title: "Animal",
+  },
+  {
+    icon: "",
+    title: "Feeding",
+  },
+];
