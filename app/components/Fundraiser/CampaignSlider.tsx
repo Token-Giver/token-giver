4f468f2fd55125c36fc1a2af396dabbf748a257b
sylvester <sylvesterok4@gmail.com>
2025-03-01 15:21:37 +0100

    updating...

--- app/components/Fundraiser/CampaignSlider.tsx
+++ app/components/Fundraiser/CampaignSlider.tsx
@@ -3,7 +3,7 @@ import { campaign_contract } from "@/app/utils/data";
 import { fetchCampaigns } from "@/app/utils/helper";
 import RightArrowIcon from "@/svgs/RightArrowIcon";
 import { useEffect, useState } from "react";
-import { CardLoader } from "../loading/CardLoader";
+import CardLoader from "../loading/CardLoader";
 
 const CampaignSlider = () => {
   const [collections, setCollections] = useState<any[]>([]);
