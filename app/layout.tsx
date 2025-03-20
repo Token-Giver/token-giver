0ceee2da9b49027e4b33dc877fcd3f7c775c840c
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-03-20 14:26:52 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen flex-col">
+      <body>
         <Provider>
           <Header />
           <main className="flex-1">{children}</main>
